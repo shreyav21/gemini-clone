c6cc4f54d409920b1ac4c096973623f26bdd47aa
shreya vedpathak <shreyavedpathak71@outlook.com>
2024-11-09 14:29:52 +0530

    Context

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import './Main.css'
 import { assets } from '../../assets/assets';
-import { Context } from '../../context/context';
+import { Context } from '../../Context/Context';
 
 const Main = () => {
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ContextProvider from './context/context.jsx'
+import ContextProvider from './Context/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <ContextProvider>
